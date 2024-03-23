4b47b32702d9aad1d3e537fb7b051e0104af41b3
NdegeFelix <findech6@gmail.com>
2024-03-23 09:33:30 +0300

    portfolio

---
+++ script.js
@@ -0,0 +1,8 @@
+// script.js
+
+// Add JavaScript code for interactivity, form validation, etc.
+document.getElementById('contact-form').addEventListener('submit', function(event) {
+    event.preventDefault();
+    // Add code for form submission, validation, and sending data
+  });
+  
